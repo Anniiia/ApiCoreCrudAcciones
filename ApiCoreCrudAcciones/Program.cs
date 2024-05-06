ef0fac46a40b5fe018e0fcfad2645762148c3f21
Anniiia <annaortegabueno@gmail.com>
2024-05-06 19:33:17 +0200

    commit

--- ApiCoreCrudAcciones/Program.cs
+++ ApiCoreCrudAcciones/Program.cs
@@ -1,19 +1,47 @@
 using ApiCoreCrudAcciones.Data;
 using ApiCoreCrudAcciones.Helpers;
 using ApiCoreCrudAcciones.Repositories;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Azure;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-HelperActionServicesOAuth helper = new HelperActionServicesOAuth(builder.Configuration);
+
+builder.Services.AddAzureClients(factory =>
+{
+    factory.AddSecretClient
+    (builder.Configuration.GetSection("KeyVault"));
+});
+
+
+SecretClient secretClient = builder.Services.BuildServiceProvider().GetService<SecretClient>();
+
+KeyVaultSecret secretK = await secretClient.GetSecretAsync("SecretKey"); //Aqui ponemos el nombre del secret
+KeyVaultSecret audienceKey = await secretClient.GetSecretAsync("Audience");
+KeyVaultSecret issuerKey = await secretClient.GetSecretAsync("Issuer");
+
+string secretKey = secretK.Value;
+string audience = audienceKey.Value;
+string issuer = issuerKey.Value;
+
+HelperActionServicesOAuth helper = new HelperActionServicesOAuth(secretKey, audience, issuer);
+
+
+KeyVaultSecret secret =
+    await secretClient.GetSecretAsync("SqlAzure");
+string connectionString = secret.Value;
+
+
+//HelperActionServicesOAuth helper = new HelperActionServicesOAuth(builder.Configuration);
 builder.Services.AddSingleton<HelperActionServicesOAuth>(helper);
 builder.Services.AddAuthentication(helper.GetAuthenticateSchema()).AddJwtBearer(helper.GetJwtBearerOptions());
 
 builder.Services.AddTransient<RepositoryAcciones>();
 builder.Services.AddTransient<HelperAccion>();
-string connectionString = builder.Configuration.GetConnectionString("SqlAzure");
+//string connectionString = builder.Configuration.GetConnectionString("SqlAzure");
 builder.Services.AddDbContext<AccionesContext>(options=>options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
